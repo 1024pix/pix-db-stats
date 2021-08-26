88712321bf8e5227c8801b810692e911482adcd9
Jonathan Perret <j-github@jonathanperret.net>
2021-08-26 22:47:54 +0200

    Add progress reporting
    The new progress task will dump the contents of pg_stat_progress_* tables regularly.

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
-const DEFAULT_RESPONSE_TIME_QUERY = 'SELECT pg_sleep(1)'
+const DEFAULT_RESPONSE_TIME_QUERY = 'SELECT pg_sleep(1)';
+const DEFAULT_PROGRESS_SCHEDULE = '0 */10 * * * *';
 
 module.exports = {
   SCALINGO_REGION: process.env.SCALINGO_REGION,
@@ -9,9 +10,11 @@ module.exports = {
   FT_METRICS: process.env.FT_METRICS === 'yes',
   FT_STATEMENTS: process.env.FT_STATEMENTS === 'yes',
   FT_RESPONSE_TIME: process.env.FT_RESPONSE_TIME === 'yes',
+  FT_PROGRESS: process.env.FT_PROGRESS === 'yes',
   METRICS_SCHEDULE: process.env.METRICS_SCHEDULE,
   STATEMENTS_SCHEDULE: process.env.STATEMENTS_SCHEDULE,
   RESPONSE_TIME_SCHEDULE: process.env.RESPONSE_TIME_SCHEDULE,
   RESPONSE_TIME_QUERY: process.env.RESPONSE_TIME_QUERY || DEFAULT_RESPONSE_TIME_QUERY ,
   MONITORED_DATABASE_URL: process.env.MONITORED_DATABASE_URL,
+  PROGRESS_SCHEDULE: process.env.PROGRESS_SCHEDULE || DEFAULT_PROGRESS_SCHEDULE,
 };
