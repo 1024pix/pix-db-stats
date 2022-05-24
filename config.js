51d125fe6ab3a7fd9f6b5e3d69720be6c04975a0
François de Metz <francois.de-metz@pix.fr>
2022-05-24 11:25:36 +0200

    Add _isFeatureEnabled function

--- config.js
+++ config.js
@@ -3,14 +3,18 @@ require('dotenv').config();
 const DEFAULT_RESPONSE_TIME_QUERY = 'SELECT pg_sleep(1)';
 const DEFAULT_PROGRESS_SCHEDULE = '0 */10 * * * *';
 
+function _isFeatureEnabled(valueString) {
+  return valueString === 'yes';
+}
+
 module.exports = {
   SCALINGO_REGION: process.env.SCALINGO_REGION,
   SCALINGO_APP: process.env.SCALINGO_APP,
   SCALINGO_TOKEN: process.env.SCALINGO_TOKEN,
-  FT_METRICS: process.env.FT_METRICS === 'yes',
-  FT_STATEMENTS: process.env.FT_STATEMENTS === 'yes',
-  FT_RESPONSE_TIME: process.env.FT_RESPONSE_TIME === 'yes',
-  FT_PROGRESS: process.env.FT_PROGRESS === 'yes',
+  FT_METRICS: _isFeatureEnabled(process.env.FT_METRICS),
+  FT_STATEMENTS: _isFeatureEnabled(process.env.FT_STATEMENTS),
+  FT_RESPONSE_TIME: _isFeatureEnabled(process.env.FT_RESPONSE_TIME),
+  FT_PROGRESS: _isFeatureEnabled(process.env.FT_PROGRESS),
   METRICS_SCHEDULE: process.env.METRICS_SCHEDULE,
   STATEMENTS_SCHEDULE: process.env.STATEMENTS_SCHEDULE,
   RESPONSE_TIME_SCHEDULE: process.env.RESPONSE_TIME_SCHEDULE,
