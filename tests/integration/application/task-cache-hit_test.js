da6d362c518184fe870e556a38f9fe5ae1fd114a
Yoan de LUCA <yoan.de-luca@pix.fr>
2022-10-10 15:04:28 +0200

    adding error handling for tasks in logger

--- tests/integration/application/task-cache-hit_test.js
+++ tests/integration/application/task-cache-hit_test.js
@@ -1,5 +1,5 @@
-const { expect } = require('chai');
-const { getCacheHit } = require('../../../lib/application/task-cache-hit');
+const { expect, nock } = require('../../test-helper');
+const { getCacheHit, logCacheHits } = require('../../../lib/application/task-cache-hit');
 const { TEST_DATABASE_URL: connexionString } = require('../../../config');
 
 describe('#getCacheHit', function () {
@@ -11,5 +11,25 @@ describe('#getCacheHit', function () {
     expect(result).to.be.a('number');
     expect(result).to.be.at.least(0);
     expect(result).to.be.at.most(100);
+  });
+});
+
+describe('#logCacheHits', function () {
+  it('should not throw an error when API Scalingo fails', async function () {
+    let hasThrown = false;
+    // when
+    nock(`https://auth.scalingo.com`).persist().post('/v1/tokens/exchange').reply(401, {
+      token: 'myfaketoken',
+      error: 'Invalid credentials',
+    });
+
+    try {
+      await logCacheHits();
+    } catch (error) {
+      console.log(error);
+      hasThrown = true;
+    }
+    // then
+    expect(hasThrown).to.be.false;
   });
 });
